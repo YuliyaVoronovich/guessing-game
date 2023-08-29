19e1679df544e145d5c988dd8c9b49989cb07739
YuliyaVoronovich <Y-1306@mail.ru>
2023-08-29 17:53:34 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
+
+    average = 0;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.ceil(this.min/2 + this.max/2);
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average;
     }
 
     greater() {
-
+        this.min = this.average;
     }
 }
 
